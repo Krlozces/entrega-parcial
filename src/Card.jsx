3c85104b436ff925b11a7d895fe8dfa7fee4dd99
Brayan Cespedes <bryancespedes1706@gmail.com>
2024-11-07 20:32:08 -0500

    Card component created

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,10 +1,18 @@
 //Este componente deberia recibir por props y mostrar en pantalla la informacion
 //que envia el usuario
 
-function Card() {
+function Card({ placa, name, brand, year, maxspeed, price }) {
   return (
     <div>
-      <h2>Esto es un componente</h2>
+      <div className="card-information">
+      <h2>Información del Carro F1</h2>
+      <p><strong>Placa:</strong> {placa}</p>
+      <p><strong>Nombre:</strong> {name}</p>
+      <p><strong>Marca:</strong> {brand}</p>
+      <p><strong>Año:</strong> {year}</p>
+      <p><strong>Velocidad máxima:</strong> {maxspeed} km/h</p>
+      <p><strong>Precio:</strong> ${price}</p>
+    </div>
     </div>
   );
 }
